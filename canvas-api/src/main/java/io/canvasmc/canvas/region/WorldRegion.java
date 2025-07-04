2c78274905274d39932a3f7514792571b7004de2
Dueris <jedimastertoothless@hotmail.com>
2025-07-03 22:05:51 -0700

    Expand region API

---
+++ canvas-api/src/main/java/io/canvasmc/canvas/region/WorldRegion.java
@@ -0,0 +1,68 @@
+package io.canvasmc.canvas.region;
+
+import org.bukkit.Chunk;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import java.util.List;
+
+/**
+ * Represents a Folia/Canvas region
+ */
+public interface WorldRegion {
+
+    /**
+     * Gets the world this region belongs to.
+     *
+     * @return the associated {@link World}
+     */
+    World getWorld();
+
+    /**
+     * Retrieves the center {@link Chunk}
+     *
+     * @return center chunk of this region
+     */
+    Chunk getCenterChunk();
+
+    /**
+     * Lists all owned chunks
+     *
+     * @return owned chunks within this region
+     */
+    List<Chunk> getOwnedChunks();
+
+    /**
+     * Lists currently ticking chunks in this region.
+     *
+     * @return list of ticking chunks
+     */
+    List<Chunk> getTickingChunks();
+
+    /**
+     * Number of chunks owned by this region.
+     *
+     * @return total owned chunks count
+     */
+    int getChunkCount();
+
+    /**
+     * Lists all local players
+     *
+     * @return players present in this region
+     */
+    List<Player> getLocalPlayers();
+
+    /**
+     * Count of players currently inside this region.
+     *
+     * @return number of players
+     */
+    int getPlayerCount();
+
+    /**
+     * Count of entities currently loaded in the region
+     *
+     * @return number of entities
+     */
+    int getEntityCount();
+}
