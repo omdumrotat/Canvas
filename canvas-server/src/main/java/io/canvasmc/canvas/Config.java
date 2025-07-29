126fce4bcfa990aeeae708221cdf91a5c5bf88b4
Dueris <jedimastertoothless@hotmail.com>
2025-07-29 10:47:19 -0700

    Pufferfish SIMD

--- canvas-server/src/main/java/io/canvasmc/canvas/Config.java
+++ canvas-server/src/main/java/io/canvasmc/canvas/Config.java
@@ -11,10 +11,10 @@ import io.canvasmc.canvas.config.ConfigurationUtils;
 import io.canvasmc.canvas.config.RuntimeModifier;
 import io.canvasmc.canvas.config.SerializationBuilder;
 import io.canvasmc.canvas.config.annotation.Comment;
-import io.canvasmc.canvas.config.annotation.Experimental;
 import io.canvasmc.canvas.config.annotation.NamespacedKey;
 import io.canvasmc.canvas.config.internal.ConfigurationManager;
 import io.canvasmc.canvas.entity.EntityCollisionMode;
+import io.canvasmc.canvas.simd.SIMDDetection;
 import io.canvasmc.canvas.util.YamlTextFormatter;
 import java.util.LinkedHashSet;
 import java.util.Set;
@@ -463,6 +463,22 @@ public class Config {
                 }
                 for (final String change : changes) {
                     LOGGER.info(change);
+                }
+
+                // SIMD
+                try {
+                    SIMDDetection.isEnabled = SIMDDetection.canEnable(LOGGER);
+                } catch (NoClassDefFoundError | Exception ignored) {
+                    ignored.printStackTrace();
+                }
+
+                if (SIMDDetection.isEnabled) {
+                    LOGGER.info("SIMD operations detected as functional. Will replace some operations with faster versions.");
+                } else {
+                    LOGGER.warn("SIMD operations are available for your server, but are not configured!");
+                    LOGGER.warn("To enable additional optimizations, add \"--add-modules=jdk.incubator.vector\" to your startup flags, BEFORE the \"-jar\".");
+                    LOGGER.warn("If you have already added this flag, then SIMD operations are not supported on your JVM or CPU.");
+                    LOGGER.warn("Debug: Java: " + System.getProperty("java.version") + ", test run: " + SIMDDetection.testRun);
                 }
             })
             .build(config, configClass), changes::add
