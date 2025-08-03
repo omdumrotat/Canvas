fa9406b58a52ecb1f9ee522a3c6348e6dc5faec5
Dueris <jedimastertoothless@hotmail.com>
2025-08-03 12:40:36 -0700

    Optimize BlockEntity Fetching

--- canvas-test-plugin/src/main/java/io/canvasmc/testplugin/TestPlugin.java
+++ canvas-test-plugin/src/main/java/io/canvasmc/testplugin/TestPlugin.java
@@ -5,15 +5,24 @@ import java.util.List;
 import java.util.Objects;
 import io.canvasmc.canvas.event.WorldPreLoadEvent;
 import net.kyori.adventure.util.TriState;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.RandomSource;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.chunk.ChunkAccess;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.WorldCreator;
 import org.bukkit.WorldType;
 import org.bukkit.block.Biome;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
+import org.bukkit.craftbukkit.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -43,6 +52,58 @@ public class TestPlugin extends JavaPlugin implements Listener {
                             player.getWorld(), blockX, 90, blockZ, player.getYaw(), player.getPitch()
                         )
                     );
+                    return true;
+                }
+                return false;
+            }
+        });
+        getServer().getCommandMap().register("blockentitytest", new BukkitCommand("blockentitytest") {
+            @Override
+            public boolean execute(@NotNull final CommandSender sender, @NotNull final String commandLabel, final @NotNull String @NotNull [] args) {
+                if (sender instanceof Player bukkitPlayer) {
+                    ServerPlayer serverPlayer = ((CraftPlayer) bukkitPlayer).getHandle();
+                    ServerLevel world = serverPlayer.level();
+                    final int chunkX = serverPlayer.chunkPosition().x;
+                    final int chunkZ = serverPlayer.chunkPosition().z;
+                    ChunkAccess nmsChunk = world.getChunk(chunkX, chunkZ);
+                    int startX = (chunkX << 4) - 2;
+                    int endX = (chunkX << 4) + 16 + 1;
+                    int startZ = (chunkZ << 4) - 2;
+                    int endZ = (chunkZ << 4) + 16 + 1;
+                    int minY = world.getWorld().getMinHeight();
+                    int maxY = world.getWorld().getMaxHeight();
+
+                    for (int x = startX; x <= endX; x++) {
+                        for (int z = startZ; z <= endZ; z++) {
+                            for (int y = minY; y < maxY; y++) {
+                                world.setBlock(new BlockPos(x, y, z), Blocks.AIR.defaultBlockState(), 3);
+                            }
+                        }
+                    }
+
+                    int baseX = chunkX << 4;
+                    int baseZ = chunkZ << 4;
+
+                    BlockData hopperData = Bukkit.createBlockData(Material.HOPPER);
+                    BlockData chestData = Bukkit.createBlockData(Material.CHEST);
+
+                    for (int x = baseX; x < baseX + 16; x++) {
+                        for (int z = baseZ; z < baseZ + 16; z++) {
+                            world.getWorld().setBlockData(x, minY, z, chestData);
+                            BlockEntity be1 = nmsChunk.getFromBuckets(x, minY, z);
+                            if (be1 == null) throw new IllegalStateException("Failed to fetch block entity from buckets");
+                            BlockPos chestPos = new BlockPos(x, minY, z);
+                            if (!be1.getBlockPos().equals(chestPos)) throw new IllegalStateException("Mismatched block entity position");
+
+                            for (int y = minY + 1; y < maxY; y++) {
+                                world.getWorld().setBlockData(x, y, z, hopperData);
+                                BlockEntity be2 = nmsChunk.getFromBuckets(x, y, z);
+                                if (be2 == null) throw new IllegalStateException("Failed to fetch block entity from buckets");
+                                BlockPos aPos = new BlockPos(x, y, z);
+                                if (!be2.getBlockPos().equals(aPos)) throw new IllegalStateException("Mismatched block entity position");
+                            }
+                        }
+                    }
                     return true;
                 }
                 return false;
